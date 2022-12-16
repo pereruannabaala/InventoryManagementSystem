4111271cb913b2d148d4a3b33e9d3345bc1a4268
pereruan <pereruanabaala@gmail.com>
2022-12-16 17:51:20 +0300

    add flask

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+ 
