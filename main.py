b4105ffaa0186aaacb8bf4694ae9a61203a02bab
pereruan <pereruanabaala@gmail.com>
2022-12-19 20:22:10 +0300

    add jinja

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 
+import psycopg2
+
 app = Flask(__name__)
 
+conn=psycopg2.connect("host='localhost' user='postgres' password='1234' dbname='dukalangu'")
 
 @app.route('/')
 def home():
@@ -9,11 +12,23 @@ def home():
 
 @app.route('/products')
 def products():
-    return render_template('products.html')
+    cur=conn.cursor()
+    q="SELECT * FROM products;"
+    cur.execute(q)
+    r=cur.fetchall()
+    print(r)
+    return render_template('products.html',rows=r)
+    
+    
 
 @app.route('/sales')
 def sales():
-    return render_template('sales.html')
+    cur=conn.cursor()
+    p="SELECT * FROM sales;"
+    cur.execute(p)
+    s=cur.fetchall()
+    print(s)
+    return render_template('sales.html', sales=s)
 
 
 
